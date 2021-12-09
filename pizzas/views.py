bd7093acf934753eff4b42b2c8a5ffa69d4dcc7d
Zobia Mohsin <zobia_mohsin1@baylor.edu>
2021-12-09 12:54:33 -0600

    Created toppings Topic and restrcted login to user

--- pizzas/views.py
+++ pizzas/views.py
@@ -5,3 +5,68 @@ from django.contrib.auth.decorators import login_required
 from django.http import Http404
 
 # Create your views here.
+def index(request):
+    return render(request, 'Pizzas/index.html')
+
+
+
+@login_required
+def pizzas(request):
+    pizzas = Pizza.objects.order_by('date')
+
+
+    context = {'pizzas':pizzas}
+    return render(request, 'pizzas/pizzas.html', context)
+
+
+@login_required
+def pizza(request, pizza_id):
+    pizza = Pizza.objects.get(id=pizza_id)
+    toppings = pizza.toppings_set.order_by('-date_added')
+    comments = pizza.comment_set.order_by('-date_added')
+
+    context = {'pizza': pizza, 'toppings': toppings, 'comments': comments}
+    return render(request, 'pizzas/pizza.html', context)
+
+
+@login_required
+def new_pizza(request):
+    if request.method != 'POST':
+        form = PizzaForm()
+
+    else:
+        form = PizzaForm(data=request.POST)
+
+
+        if form.is_valid():
+            new_pizza = form.save(commit=False)
+            new_pizza.owner = request.user
+            form.save()
+        
+
+            return redirect('pizzas:pizzas')
+    context = {'form':form}
+    return render(request, 'pizzas/new_pizza.html', context)
+
+
+@login_required
+def new_topping(request, pizza_id):
+    pizza = Pizza.objects.get(id=pizza_id)
+
+    if request.method != 'POST':
+        form = ToppingForm()
+
+    else:
+        form = ToppingForm(data=request.POST)
+
+        if form.is_valid():
+            new_topping = form.save(commit=False)
+            new_topping.pizza = pizza
+            new_topping.owner = request.user 
+            new_topping.save()
+
+            return redirect('pizzas:pizza', pizza_id=pizza_id)
+
+
+    context = {'form':form, 'pizza':pizza}
+    return render(request, 'pizzas/new_topping.html', context)
