6e9ba1aa6c07d8632cc77d1aa7e21a70c237c7f2
Zobia Mohsin <zobia_mohsin1@baylor.edu>
2021-12-09 13:22:04 -0600

    added pizzas, users to installed apps settings

---
+++ pizzas/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path 
+from . import views
+
+
+app_name = 'pizzas'
+
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('pizzas', views.pizzas, name='pizzas'),
+    path('pizzas/<int:pizza_id>/', views.pizza, name='pizza'), 
+    path('new_pizza/', views.new_pizza, name='new_pizza'),
+    path('new_topping/<int:pizza_id>/', views.new_topping, name='new_topping'),
+    path('edit_topping/<int:topping_id>/', views.edit_topping, name='edit_topping'),
+    path('new_comment/<int:pizza_id>/', views.new_comment, name='new_comment'),
+  
+]
--- pizzas/views.py
+++ pizzas/views.py
@@ -70,3 +70,47 @@ def new_topping(request, pizza_id):
 
     context = {'form':form, 'pizza':pizza}
     return render(request, 'pizzas/new_topping.html', context)
+    
+@login_required
+def edit_topping(request, topping_id):
+    topping = Topping.objects.get(id=topping_id)
+    pizza = topping.pizza
+
+
+    if pizza.owner != request.user:
+        raise Http404
+    if request.method != 'POST':
+        form = ToppingForm(instance=topping)
+    else:
+        form = ToppingForm(instance=topping, data=request.POST)
+
+
+        if form.is_valid():
+            form.save()
+            return redirect('pizzas:pizza', pizza_id=pizza.id)
+
+    context = {'topping':topping, 'pizza':pizza, 'form':form}
+    return render(request, 'pizzas/edit_topping.html', context)
+
+
+
+@login_required
+def new_comment(request, pizza_id):
+    pizza = Pizza.objects.get(id=pizza_id)
+
+
+    if request.method != 'POST':
+        form = CommentForm()
+
+    else:
+        form = CommentForm(data=request.POST)
+
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.pizza = pizza
+            new_comment.owner = request.user
+            new_comment.save()
+            return redirect('pizzas:pizza', pizza_id=pizza_id)
+
+    context = {'form':form, 'pizza':pizza}
+    return render(request, 'pizzas/new_comment.html', context)
