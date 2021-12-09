41c3265251cc1a8f9fb492fa3cf4b51dce0553d4
Zobia Mohsin <zobia_mohsin1@baylor.edu>
2021-12-09 13:02:02 -0600

    sub classes of all topics (functions).

---
+++ pizzas/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from .models import Pizza, Toppings, Comment 
+
+
+class PizzaForm(forms.ModelForm):
+    class Meta:
+        model = Pizza
+        fields = ['name']
+        labels = {'name':''}
+
+
+
+class ToppingForm(forms.ModelForm):
+    class Meta:
+        model = Toppings
+        fields = ['name']
+        labels = {'name':''}
+
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['name']
+        labels = {'name':''}
